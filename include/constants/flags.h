904755f99c25b8ee898dc90f48e71b435f60e140
JammyStufff <88464669+JammyStufff@users.noreply.github.com>
2024-10-02 12:58:48 -0400

    im going to boil myself

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -43,7 +43,7 @@
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
 #define FLAG_CELEBI_0x020    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
+#define FLAG_MET_PROF    0x21 // Unused Flag
 #define FLAG_UNUSED_0x022    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
