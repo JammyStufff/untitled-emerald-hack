0950924b3c6a8fbf50e818ca3abb7139ee182ce7
JammyStufff <88464669+JammyStufff@users.noreply.github.com>
2024-10-22 12:54:32 -0400

    making work

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -72,11 +72,12 @@
 #define MAP_LITTLEROOT_TOWN_PROFESSOR_BIRCHS_LAB (4 | (1 << 8))
 
 // gMapGroup_IndoorOldale
-#define MAP_OLDALE_TOWN_HOUSE1            (0 | (2 << 8))
-#define MAP_OLDALE_TOWN_HOUSE2            (1 | (2 << 8))
-#define MAP_OLDALE_TOWN_POKEMON_CENTER_1F (2 | (2 << 8))
-#define MAP_OLDALE_TOWN_POKEMON_CENTER_2F (3 | (2 << 8))
-#define MAP_OLDALE_TOWN_MART              (4 | (2 << 8))
+#define MAP_OLDALE_TOWN_HOUSE1                  (0 | (2 << 8))
+#define MAP_OLDALE_TOWN_HOUSE2                  (1 | (2 << 8))
+#define MAP_OLDALE_TOWN_POKEMON_CENTER_1F       (2 | (2 << 8))
+#define MAP_OLDALE_TOWN_POKEMON_CENTER_2F       (3 | (2 << 8))
+#define MAP_OLDALE_TOWN_MART                    (4 | (2 << 8))
+#define MAP_OLDALE_TOWN_SCAVENGER_HUNT_BUILDING (5 | (2 << 8))
 
 // gMapGroup_IndoorDewford
 #define MAP_DEWFORD_TOWN_HOUSE1            (0 | (3 << 8))
